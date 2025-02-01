3e96c9d5c8c3df099bb6b7ca327763564af1c237
RaminaSh <90727934+RaminaSH@users.noreply.github.com>
2025-02-01 23:31:00 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 import requests
+import datetime
+
+#from bs4 import BeautifulSoup
 
 def check_url_availability(url):
     try:
@@ -9,8 +12,9 @@ def check_url_availability(url):
         print(f"Error checking {url}: {e}")
         return False
 
-def check_series(urls_file, name):
+def check_series(urls_file, name,sal):
     first_series = name[0].upper()
+    first_serieslow = name[0].lower()
     with open(urls_file, 'r') as file:
         domains = file.readlines()
 
@@ -25,7 +29,23 @@ def check_series(urls_file, name):
     for domain in domains:
         domain = domain.strip()
         if domain:  # Check if the line is not empty
+            url = f"https://{domain}/{sal}/{name}"
+            is_available = check_url_availability(url)
+            if is_available:
+                print(f"Subdomain URL: {url} is available")
+
+    for domain in domains:
+        domain = domain.strip()
+        if domain:  # Check if the line is not empty
             url = f"https://{domain}/{first_series}/{name}"
+            is_available = check_url_availability(url)
+            if is_available:
+                print(f"Subdomain URL: {url} is available")
+
+    for domain in domains:
+        domain = domain.strip()
+        if domain:  # Check if the line is not empty
+            url = f"https://{domain}/{first_serieslow}/{name}"
             is_available = check_url_availability(url)
             if is_available:
                 print(f"Subdomain URL: {url} is available")
@@ -44,7 +64,32 @@ def search_movie_in_page(url, movie_name):
 def check_film(urls_file, name, sub, sal):
     with open(urls_file, 'r') as file:
         domains = file.readlines()
+    first_film = name[0].upper()
+    datenow = datetime.datetime.now()
     if sub:
+        for domain in domains:
+                domain = domain.strip()
+                if domain == "cdn.bolbolk.fun/Film":  # Check if the line is not empty
+                    url = f"https://{domain}/SOFT.SUB/{first_film}"
+                    found = search_movie_in_page(url, name)
+                    if found:
+                        print(f"Movie name found in {url}")
+
+        for domain in domains:
+            domain = domain.strip()
+            if domain == "vip10.meserverpro.lol/dl32/Film":
+                found_flag = False
+                for i in range(1, datenow.month + 1):
+                    if found_flag:
+                        break
+                    for j in range(1, 32):
+                        url = f"https://{domain}/SoftSub/2025/{'%02d' % i}/{'%02d' % j}"
+                        found = search_movie_in_page(url, name)
+                        if found:
+                            print(f"Movie name found in {url}")
+                            found_flag = True
+                            break
+
         for domain in domains:
             domain = domain.strip()
             if domain:  # Check if the line is not empty
@@ -52,6 +97,15 @@ def check_film(urls_file, name, sub, sal):
                 found = search_movie_in_page(url, name)
                 if found:
                     print(f"Movie name found in {url}")
+
+        for domain in domains:
+            domain = domain.strip()
+            if domain:  # Check if the line is not empty
+                url = f"https://{domain}/{sal}"
+                found = search_movie_in_page(url, name)
+                if found:
+                    print(f"Movie name found in {url}")
+
         for domain in domains:
             domain = domain.strip()
             if domain:  # Check if the line is not empty
@@ -59,6 +113,7 @@ def check_film(urls_file, name, sub, sal):
                 found = search_movie_in_page(url, name)
                 if found:
                     print(f"Movie name found in {url}")
+
         for domain in domains:
             domain = domain.strip()
             if domain:  # Check if the line is not empty
@@ -66,6 +121,8 @@ def check_film(urls_file, name, sub, sal):
                 found = search_movie_in_page(url, name)
                 if found:
                     print(f"Movie name found in {url}")
+
+
 
     else:
         for domain in domains:
@@ -77,6 +134,8 @@ def check_film(urls_file, name, sub, sal):
                     print(f"Movie name found in {url}")
 
 
+
+
 # Specify the path to your domain text file
 
 # Specify the subdomain to check
@@ -85,7 +144,8 @@ name = input("input name: ").replace(" ", ".").strip()
 video_type = input("Enter Video type: Series[0] OR Film[1]: ")
 if video_type == "0":
     urls_file = 'series.txt'
-    check_series(urls_file, name)
+    sal = input("Input the year: ")
+    check_series(urls_file, name, sal)
 elif video_type == "1":
     urls_file = 'films.txt'
     year = input("Enter The Year: ")
